99ef4081e114c0ac923ae7b2a40307d52550a46c
AnkitMjolnir <ankit@global.com>
2025-07-08 21:48:21 +0530

    Init project

---
+++ src/main/java/com/ankit/multithreadedServer/MultithreadedServerApplication.java
@@ -0,0 +1,13 @@
+package com.ankit.multithreadedServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadedServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadedServerApplication.class, args);
+	}
+
+}
